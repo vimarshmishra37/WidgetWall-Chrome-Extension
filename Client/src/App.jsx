7f3ea110eea463912667d7492cd2c395c25af571
aryanploxxx <aryangupta8291@gmail.com>
2024-07-25 23:48:12 +0530

    sheet

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -16,6 +16,7 @@ import Pomodoro from './Components/pomodoro/Pomodoro'
 import Calender from './Components/Calender/Calender';
 import Quote from './Components/Quote/Quote';
 import DailyStepCount from './Components/Steps/DailyStepCount';
+import Sheets from './Components/Sheets/Sheets';
 
 
 // eslint-disable-next-line react/prop-types
@@ -82,6 +83,15 @@ function App() {
           <div className='w-[97.7%] flex justify-between'>
             <div className='w-[30%] p-5 rounded-xl border bg-white flex justify-center items-center shadow-lg'>
               <Player />
+            </div>
+            <div className='w-[31%] rounded-xl border bg-white flex justify-center items-center shadow-lg'>
+              <DailyStepCount />
+            </div>
+            <div className='w-[30%] rounded-xl border bg-white flex justify-center items-center shadow-lg'><Calender /></div>
+          </div>
+          <div className='w-[97.7%] flex justify-between'>
+            <div className='w-[30%] p-5 rounded-xl border bg-white flex justify-center items-center shadow-lg'>
+              <Sheets />
             </div>
             <div className='w-[31%] rounded-xl border bg-white flex justify-center items-center shadow-lg'>
               <DailyStepCount />
