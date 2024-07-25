a1a0cb901babb294504b38222d9acc1ea5d9ebd5
aryanploxxx <144466200+vimarshmishra37@users.noreply.github.com>
2024-07-25 23:52:53 +0530

    check

--- Client/src/Components/Sheets/Sheets.jsx
+++ Client/src/Components/Sheets/Sheets.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sheets = () => {
   return (
-    <div><iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSKs3rlWgtfsEYZQG16OlGe3B7Slzy3ha9KwXReoLAsqdgLZV-BOlVHXVcJ4HqxNy2gMSXcAJZ92Eb2/pubhtml"  style={{border:'1px', borderColor: '#777'}} height="600" frameBorder="0" scrolling="no"></iframe></div>
+    <div><iframe src="ttps://docs.google.com/spreadsheets/d/e/2PACX-1vSKs3rlWgtfsEYZQG16OlGe3B7Slzy3ha9KwXReoLAsqdgLZV-BOlVHXVcJ4HqxNy2gMSXcAJZ92Eb2/pubhtml"  style={{border:'1px', borderColor: '#777'}} height="600" frameBorder="0" scrolling="no"></iframe></div>
   )
 }
 
