f3097b844d1577d45e98d78ec8280dca2fd5310f
aryanploxxx <aryangupta8291@gmail.com>
2024-07-25 23:41:21 +0530

    Sheets

---
+++ Client/src/Components/Sheets/Sheets.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sheets = () => {
+  return (
+    <div><iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSKs3rlWgtfsEYZQG16OlGe3B7Slzy3ha9KwXReoLAsqdgLZV-BOlVHXVcJ4HqxNy2gMSXcAJZ92Eb2/pubhtml?gid=1340656051&single=true"  style={{border:'1px', borderColor: '#777'}} width="400" height="600" frameBorder="0" scrolling="no"></iframe></div>
+  )
+}
+
+export default Sheets
